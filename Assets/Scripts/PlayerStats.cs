3e6554f65e5390dccd696a592a1db3e7b2e1c206
Will Homan <will.homan@hotmail.com>
2016-12-20 07:27:01 -0800

    copy from previous project

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	static public int Motivation;
+	static public int CodingSkill;
+
+	static public int Algorithms;
+
+	static public int PlatformSkill;
+
+	static public int ProjectCompletion;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		Motivation = 10;
+		Algorithms = 0;
+		PlatformSkill = 0;
+		ProjectCompletion = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
