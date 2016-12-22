ef1b3916d87e566b23c432e8193c98a18a6642ed
jackhyder <jackhyder20@gmail.com>
2016-12-22 15:58:07 -0800

    Added new dialogue and math for stat tracking

---
+++ Assets/Scripts/AlgorithmStuff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlgorithmStuff : MonoBehaviour {
+
+    public int AlgorithmPlusPlus;
+
+    public int Time;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D()
+    {
+        PlayerStats.ChangeAlgorithms(AlgorithmPlusPlus);
+        PlayerStats.ChangeTime(Time);
+    }
+}
